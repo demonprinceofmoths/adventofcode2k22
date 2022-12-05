88902322724e1474b39ca02567ebedf3a076a677
Castor Grey <caspngrey@gmail.com>
2022-12-05 12:57:30 -0500

    day 3 part 2

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Day 1 - yay ezpz
 Day 2 - part 2 doesn't work but it works for the test data i'm sobbing TwT
 Day 3 - i don't really want to mess with the dictionary right now there's gotta be an easier way
  - weather update: found the easier way
+ - part 2 also a doozy
 Day 4 -
--- rucksack.py
+++ rucksack.py
@@ -1,5 +1,7 @@
 def main():
-    sum = 0
+    sum_one = 0
+    sum_two = 0
+    group = []
     print("parsing data...")
     with open("rucksack_data.txt") as f:
         while True:
@@ -7,19 +9,33 @@ def main():
             if data == "":
                 break
             else:
+                if len(group) < 3:
+                    group.append(data)
                 first = data[:len(data)//2]
                 second = data[len(data)//2:]
                 dupe = find_dupes(first, second)
                 #print("Dupe: " + str(dupe))
                 priority = prioritize(dupe)
                 #print("Priority: " + str(priority))
-                sum += priority
-    print("Sum: " + str(sum))
+                sum_one += priority
+                if len(group) == 3:
+                    dupe_two = find_dupes_three(group.pop(), group.pop(), group.pop())
+                    priority = prioritize(dupe_two)
+                    sum_two += priority
+    print("Sum One: " + str(sum_one))
+    print("Sum Two: " + str(sum_two))
 def find_dupes(a, b):
     for x in range(len(a)):
         for y in range(len(b)):
             if a[x] == b[y]:
                 return a[x]
+
+def find_dupes_three(a, b, c):
+    for x in range(len(a)):
+        for y in range(len(b)):
+            for z in range(len(c)):
+                if a[x] == b[y] == c[z]:
+                    return a[x]
 
 def prioritize(letter):
     alpha = "abcdefghijklmnopqrstuvwxyz"
